3bb1c31e0e3f6e9e05d613eb99455628df919fc2
Alena Vinogradova <alchesh42@gmail.com>
2022-06-24 14:11:17 +0300

    add homework first lesson again

---
+++ Ex/Home01/Home102/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите два числа:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine(a + " больше " + b);
+}
+else if (a == b) 
+Console.WriteLine(a + " равно " + b);
+else
+{
+    Console.WriteLine(b + " больше " + a);
+}
+
+//Console.WriteLine($"{a} больше {b}");?
---
+++ Ex/Home01/Home106/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число 
+//и выдаёт, является ли число чётным
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if(n % 2== 0)
+Console.WriteLine("Число четное");
+
+else
+Console.WriteLine("Число нечетное");
