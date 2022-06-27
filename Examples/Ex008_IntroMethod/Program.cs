222ca0016a74c73690861854edfdaa90175a8726
Alena Vinogradova <alchesh42@gmail.com>
2022-06-27 10:15:19 +0300

    use method

--- Examples/Ex008_IntroMethod/Program.cs
+++ Examples/Ex008_IntroMethod/Program.cs
@@ -1,4 +1,4 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿int Max(int arg1, int arg2, int arg3) // тип Имя (тип ИмяАргументф, ...)
 {
     int result = arg1;
     if(arg2 > result) result = arg2;
@@ -7,7 +7,7 @@
 }
 
 int a1 = 15;
-int b1 = 90;
+int b1 = 10;
 int c1 = 41;
 int a2 = 23;
 int b2 = 25;
@@ -16,10 +16,11 @@ int a3 = 55;
 int b3 = 88;
 int c3 = 23;
 
-int max1 = Max(a1, b1, c1);
+/* int max1 = Max(a1, b1, c1);
 int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
+int max3 = Max(a3, b3, c3); */
+// int max = Max(max1, max2, max3);
 
-int max = Max(max1, max2, max3);
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
