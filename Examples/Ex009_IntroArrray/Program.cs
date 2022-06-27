bdf3cf161350251fa4e8c2126ee7afc1570e2b5b
Alena Vinogradova <alchesh42@gmail.com>
2022-06-27 12:06:48 +0300

    use method with array

--- Examples/Ex009_IntroArrray/Program.cs
+++ Examples/Ex009_IntroArrray/Program.cs
@@ -6,8 +6,12 @@
     return result;
 }
 //              0    1   2   3   4   5   6   7   8
-int [] array = {11, 21, 31, 44, 25, 61, 17, 28, 19};
-array[0] = 13;                  //перезапись ячейки массива
-Console.WriteLine(array[0]);    //вывод ячейки массива
+int [] array = {111, 21, 31, 44, 25, 61, 17, 28, 19};
+
+int max = Max(  Max(array[0], array[1], array[2]), 
+                Max(array[3], array[4], array[5]), 
+                Max(array[6], array[7], array[8])
+                );
 
 
+Console.WriteLine(max);
